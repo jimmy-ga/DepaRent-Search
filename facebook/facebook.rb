c70b6258e7929715829a57a595c417cf5e6da604
Esteban Leandro <elndro85@gmail.com>
2014-06-10 01:37:00 -0600

    cambio nombre

---
+++ facebook/facebook.rb
@@ -0,0 +1,71 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'omniauth'
+require 'omniauth-github'
+require 'omniauth-facebook'
+require 'omniauth-twitter'
+#TODO require 'omniauth-att'
+
+class SinatraApp < Sinatra::Base
+  configure do
+    set :sessions, true
+    set :inline_templates, true
+  end
+  use OmniAuth::Builder do
+    provider :facebook, '475837809228613','f76490e12fd6972ae282591b82bbaced'
+    provider :twitter, 'cO23zABqRXQpkmAXa8MRw', 'TwtroETQ6sEDWW8HEgt0CUWxTavwFcMgAwqHdb0k1M'
+    #provider :att, 'client_id', 'client_secret', :callback_url => (ENV['BASE_DOMAIN']
+
+  end
+  
+  get '/' do
+    erb "
+    <a href='http://localhost:4567/auth/facebook'>Login with facebook</a><br>
+    <a href='http://localhost:4567/auth/twitter'>Login with twitter</a><br>
+    "
+  end
+  
+  get '/auth/:provider/callback' do
+    erb "<h1>#{params[:provider]}</h1>
+         <pre>#{JSON.pretty_generate(request.env['omniauth.auth'])}</pre>"
+  end
+  
+  get '/auth/failure' do
+    erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+  end
+  
+  get '/auth/:provider/deauthorized' do
+    erb "#{params[:provider]} has deauthorized this app."
+  end
+  
+  get '/protected' do
+    throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+    erb "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
+         <a href='/logout'>Logout</a>"
+  end
+  
+  get '/logout' do
+    session[:authenticated] = false
+    redirect '/'
+  end
+
+end
+
+SinatraApp.run! if __FILE__ == $0
+
+__END__
+
+@@ layout
+<html>
+  <head>
+    <link href='http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css' rel='stylesheet' />
+  </head>
+  <body>
+    <div class='container'>
+      <div class='content'>
+        <%= yield %>
+      </div>
+    </div>
+  </body>
+</html>
